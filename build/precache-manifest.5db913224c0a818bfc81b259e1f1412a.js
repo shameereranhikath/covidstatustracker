ed9f04276985b7832896fbcc2bb4922980106557
Shameer Eranhikath <shmr_ekth@yahoo.com>
2020-09-11 21:38:00 +0400

    Build file is added

---
+++ build/precache-manifest.5db913224c0a818bfc81b259e1f1412a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb716f4b8a4bf76557a6559fb435b7a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28c4ff61d6d59724555c",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "5cab3f28271b5a91c22d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "28c4ff61d6d59724555c",
+    "url": "/static/js/2.fd9c0666.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fd9c0666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cab3f28271b5a91c22d",
+    "url": "/static/js/main.46eae550.chunk.js"
+  },
+  {
+    "revision": "4e8b7c73aa4184031398",
+    "url": "/static/js/runtime-main.b05b3026.js"
+  }
+]);
